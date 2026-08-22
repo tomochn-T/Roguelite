99ceb5db89c1076c1369c0aba64917e96c127be1
tomochn-T <issit402@gmail.com>
2026-08-22 15:55:00 +0900

    シェーダーの実装と レベルの実装

--- Assets/MyProject/script/InGame/Player/playerController.cs
+++ Assets/MyProject/script/InGame/Player/playerController.cs
@@ -35,6 +35,11 @@ namespace TPSRoguelite.InGame.Player
         private const float ATTACK_RANGE = 50f;
 
         /// <summary>
+        /// レベルアップエフェクトの描画時間
+        /// </summary>
+        private const float LEVEL_UP_EFFECT_DURATION = 2f;
+
+        /// <summary>
         /// 物理演算コンポーネント
         /// </summary>
         [SerializeField] private Rigidbody Rigidbody;
@@ -515,6 +520,12 @@ namespace TPSRoguelite.InGame.Player
         public void AddExp(int amount)
         {
             CurrentExp += amount;
+
+            if(CurrentExp >= RequiredExp)
+            {
+                LevelUp();
+            }
+
             UpdateExpUI();
         }
 
@@ -525,5 +536,35 @@ namespace TPSRoguelite.InGame.Player
                 expBar.value = (float)CurrentExp / (RequiredExp);
             }
         } 
+
+        private void LevelUp()
+        {
+            CurrentLevel++;
+
+            CurrentExp -= RequiredExp;
+
+            if(levelUpEffect != null)
+            {
+                levelUpEffect.Play();
+            }
+
+            ShowLevelUpTextAsync().Forget();
+        }
+
+        private async UniTaskVoid ShowLevelUpTextAsync()
+        {
+            if(levelUpText == null)
+            {
+                return;
+            }
+
+            levelUpText.enabled = true;
+            levelUpText.SetText($"Level Up!\n<size=50%>Lv.{CurrentLevel}</size>");
+
+            await UniTask.Delay(TimeSpan.FromSeconds(LEVEL_UP_EFFECT_DURATION),
+                cancellationToken: this.GetCancellationTokenOnDestroy());
+
+            levelUpText.enabled = false;
+        }
     }
 }
